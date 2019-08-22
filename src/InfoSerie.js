fa2ad04304804a6ff643942e15e6a2ff6f4ac616
Fillipe Oliveira <fillipe_martins@hotmail.com>
2019-08-22 14:46:09 -0300

    Added InfoSeries

--- src/InfoSerie.js
+++ src/InfoSerie.js
@@ -1,27 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios' //axios puxa informaçoes do servidor (apis e etc)
 import { Redirect } from 'react-router-dom'
+import { Badge } from 'reactstrap'
 
 const InfoSerie = ({ match }) => {
-    const [name, setName] = useState('')
+    const [form, setForm] = useState({})
     const [success, setSuccess] = useState(false)
-    
+    const [mode, setMode] = useState('INFO')
+    const [genres, setGenres] = useState([])
+
     const [data, setData] = useState({})
     useEffect(() => {
         axios
             .get('/api/series/' + match.params.id)            
             .then(res => {
                 setData(res.data)
+                setForm(res.data)
             })
     },[match.params.id])
 
-    const onChange = evt => {
-        setName(evt.target.value)
+    useEffect(() => {
+        axios
+            .get('/api/genres')
+            .then(res => {
+                setGenres(res.data.data)
+            })
+    })
+
+    //custom header
+    const masterHeader = {
+        height: '50vh',
+        minHeight: '500px' ,
+        backgroundImage: `url('${data.background}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
     }
+
+    const onChange = field => evt => {
+        setForm({
+            ...form,
+            [field]: evt.target.value //entre colchetes, o campo fica dinâmico
+        })
+    }
+
     const save = () => {
         axios
             .post('/api/series',{
-                name
+                form
             })
             .then(res => {
                 setSuccess(true)
@@ -34,16 +60,45 @@ const InfoSerie = ({ match }) => {
 
     return (
         <div>
+        <header style={masterHeader}>
+            <div className='h-100' style={{background: 'rgba(0,0,0,0.7)'}}>
+                <div className='h-100 container'>
+                    <div className='row h-100 align-items-center'>
+                        <div className='col-3'>
+                            <img alt={data.name} className='img-fluid img-thumbnail' src={data.poster} />
+                        </div>
+                        <div className='col-8'>
+                            <h1 className='font-weight-light text-white'>{data.name}</h1>
+                            <div className='lead text-white'>
+                                <Badge color='success'>teste</Badge>
+                                Genero: {data.genre}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div className='container'>
+            <button className='btn btn-primary' onClick={() => setMode('EDIT')}>Editar</button>
+        </div>
+        {
+            mode === 'EDIT' &&
             <div className='container'>
-                <h1>Nova Série</h1>
+                <h1>Editar Série</h1>
+                <button className='btn btn-primary' onClick={() => setMode('INFO')}>Cancelar Ediçao</button>
                 <form>
                     <div className='form-group'>
-                    <label htmlFor='name'>Nome da Série</label>
-                    <input type='text' value={name} onChange={onChange} className='form-control' id='name' placeholder='Nome da Série'/>                
-                </div>
-                <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
+                        <label htmlFor='name'>Nome da Série</label>
+                        <input type='text' value={form.name} onChange={onChange('name')} className='form-control' id='name' placeholder='Nome da Série'/>                
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='name'>Comentários</label>
+                        <input type='text' value={form.comments} onChange={onChange('comments')} className='form-control' id='name' placeholder='Nome da Série'/>                
+                    </div>
+                    <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
                 </form>
             </div>
+        }
         </div>
     ) 
   }
